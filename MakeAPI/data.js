3ff3dd68edfeff8e3884568f7d0e06ed7b7bb4d1
Deepak <deepakkumar750206@gmail.com>
2023-01-25 20:38:52 +0530

    Make a static API

---
+++ MakeAPI/data.js
@@ -0,0 +1,8 @@
+const data = [
+    {name: "Deepak", email: "abc@test.com", contact: "1234567890"},
+    {name: "Tom", email: "tom@test.com"},
+    {name: "Marry", email: "marry@test.com"},
+    {name: "Bruce", email: "bruce@test.com"}
+
+]
+module.exports = data;
