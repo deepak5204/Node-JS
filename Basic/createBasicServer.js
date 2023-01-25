91384e441e61c211617eb3dcf53150644d52c78a
Deepak <deepakkumar750206@gmail.com>
2023-01-25 14:02:18 +0530

    create basic server using http

---
+++ Basic/createBasicServer.js
@@ -0,0 +1,6 @@
+const http = require("http");  //"http" -> handle the request and response inside node js
+
+http.createServer((req, res) => {
+    res.write("<h1>Hello, This is Deepak!</h1>");
+    res.end;
+}).listen(3000);
