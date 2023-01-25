d184f651d364a4904cb7726aaa8d7f69d2dbd18b
Deepak <deepakkumar750206@gmail.com>
2023-01-25 14:46:19 +0530

    Install nodemon and use it

--- PackageBasic/index.js
+++ PackageBasic/index.js
@@ -4,4 +4,4 @@ var colors = require('colors');
  
 console.log('hello'.green); // outputs green text
 
-console.warn("Hello deepak" .blue .bgWhite);
+console.warn("Hello deepak, Welcome in Node js journey" .blue .bgWhite);
