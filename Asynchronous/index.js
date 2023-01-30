b076898f3ede7f2c32682ee02e97606d5b4499a7
Deepak <deepakkumar750206@gmail.com>
2023-01-30 09:49:14 +0530

    handle Aynchronous data

--- Asynchronous/index.js
+++ Asynchronous/index.js
@@ -11,12 +11,26 @@ setTimeout(()=>{
 console.log("third");
 
 
-//drawback of asynchronous
-let a = 10;
-let b =0;
+// //drawback of asynchronous
+// let a = 10;
+// let b =0;
 
-setTimeout(()=>{
-    b = 10;
-},2000);
+// setTimeout(()=>{
+//     b = 10;
+// },2000);
 
-console.log(a+b); //it execute before update b
+// console.log(a+b); //it execute before update b
+
+
+//handle Asynchronous data
+let p = 10;
+let q = 0;
+let waitingData = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+       resolve(20);
+    },2000)
+})
+
+waitingData.then((data)=>{
+    console.log(p+data);
+})
