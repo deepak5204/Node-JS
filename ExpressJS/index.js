ef57676e22fa26290463771bc19a12121d333d22
Deepak <deepakkumar750206@gmail.com>
2023-01-31 06:23:13 +0530

    Render HTML and JSON data

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -2,16 +2,31 @@ const express = require('express');
 const app = express();
 app.get('', (req, res)=>{
     console.log("data sent by browser >>>> "+ req.query.name);  //data come through query params0
-    res.send('Hello, Welcome '+req.query.name)
+    res.send(`
+        <h1>Welcome in home page.</h1>
+        <a href="/about">Go to about page</a>
+    `)
 });
 
 app.get('/about',(req, res)=>{
-    res.send("<h1>Hello this is about page.</h1>")
+    //render html data
+    res.send(`
+        <h1>Hello this is about page.</h1>
+        <input type="text" placeholder="User Name">    
+        <button>Click me</button>
+        <br>
+        <a href="/">Go to home page</a>
+    `)
 });
 
 
 app.get('/help',(req,res)=>{
-    res.send("Hello this is help page.")
+    // return json data
+    res.send({
+        message: "Hello this is help page.",
+        name: 'deepak',
+        email: 'abc@123.com'
+    })
 });
 
 
