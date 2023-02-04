5fee35e8e9f40c4630b5339d3ee6c4ba634974ec
Deepak <deepakkumar750206@gmail.com>
2023-02-04 16:34:03 +0530

    apply route from different file

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const reqFilter = require('./middleware');
 const app = express();
+const route = express.Router();
 
-//middleware
-const reqFilter = (req, res, next) =>{
-    if(!req.query.age){
-        res.send('Please provide age.');
-    }else if(req.query.age < 18){
-        res.send('You cannot access this websites.');
-    }
-     else{
-        next();
-    }
-}
+route.use(reqFilter);
+
+
+// //middleware
+// const reqFilter = (req, res, next) =>{
+//     if(!req.query.age){
+//         res.send('Please provide age.');
+//     }else if(req.query.age < 18){
+//         res.send('You cannot access this websites.');
+//     }
+//      else{
+//         next();
+//     }
+// }
 
 // app.use(reqFilter); //--> application level middleware
 
@@ -27,5 +32,12 @@ app.get('/users',(req, res)=>{
 app.get('/about' , reqFilter ,(req, res)=>{
     res.send("Welcome to about page");
 });
+
+
+route.get('/contact' , (req, res)=>{
+    res.send("Welcome to contact page.");
+});
+
+app.use('/', route);
 
 app.listen(5000);
